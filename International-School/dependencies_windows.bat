73569f08921836922ee0c026ff355f09b997bba1
AkshuDev <148709451+AkshuDev@users.noreply.github.com>
2024-08-24 19:27:08 +0530

    Delete International-School directory

--- International-School/dependencies_windows.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call npm install express
-call npm install body-parser
-call npm install mongoose
-call npm install bcryptjs
-call npm install express-session
-call npm install dotenv
-call npm install mongodb
