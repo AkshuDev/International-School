7b1696682a8c55153d2a881c54520eef3858af25
Jenil <96560649+CodeWithJenil@users.noreply.github.com>
2024-09-01 13:35:03 +0530

    Changed Home Page, Added Footer, Gave Credits, Corrected Welcome on HomePage

--- International-School/FrontEnd/script.js
+++ International-School/FrontEnd/script.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     var navLinks = document.querySelectorAll('.nav a:not(#NOT)');
-
+    var headerHeight = document.querySelector('.header').offsetHeight;
+  
     navLinks.forEach(function(link) {
-    link.addEventListener('click', function(event) {
-    event.preventDefault();
-    var targetId = link.getAttribute('href');
-    var target = document.querySelector(targetId);
-    target.scrollIntoView({ behavior: 'smooth' });
-
-    });window.scrollBy(0, 1000);
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+        var targetId = link.getAttribute('href');
+        var target = document.querySelector(targetId);
+        var targetPosition = target.offsetTop - headerHeight;
+        window.scrollTo({ top: targetPosition, behavior: 'smooth' });
+      });
     });
-   });
+  });
